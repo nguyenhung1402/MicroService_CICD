82a90110f700a3a066d7ef2bab7c3b8f808a03eb
nguyenhung1402 <21522118@gm.uit.edu.vn>
2024-06-30 15:17:51 +0700

    COMMIT

---
+++ user/server.js
@@ -0,0 +1,16 @@
+import app from "./app.js";
+
+import dbConnection from "./dbConfig/dbConnection.js";
+
+// MONGODB CONNECTION
+dbConnection();
+
+
+const PORT = process.env.PORT || 8800;
+
+const server = app.listen(PORT, () => {
+  console.log(`Test`);
+  console.log(`Dev Server running on port: ${PORT}`);
+});
+
+export default server;
